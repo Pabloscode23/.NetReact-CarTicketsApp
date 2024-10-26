c302661c338f455c53369598951707e6e37348b2
Jose Pablo Navarro <jnavarror@ucenfotec.ac.cr>
2024-10-25 18:08:16 -0600

    [Fullstack]: creacion de usuario por parte del admin con su rol

--- frontend/src/modules/admin/components/CreateUserForm.jsx
+++ frontend/src/modules/admin/components/CreateUserForm.jsx
@@ -1,18 +1,22 @@
 import { useRef } from "react";
 import { useForm } from "react-hook-form";
 import '../styles/CreateUserForm.css';
+import { API_URL } from "../../../constants/Api";
+import axios from "axios";
 
 const roles = [
     { value: "admin", label: "Administrador" },
     { value: "oficial", label: "Oficial" },
     { value: "juez", label: "Juez" },
-]
+];
 
 export const CreateUserForm = () => {
+
     const {
         register,
         formState: { errors },
         watch,
+        handleSubmit,
     } = useForm({
         defaultValues: {
             firstName: "",
@@ -24,24 +28,44 @@ export const CreateUserForm = () => {
             confirmPassword: "",
             role: roles[0].value,
         },
-    })
+    });
 
     const password = useRef(null);
     password.current = watch("password", "");
 
-    const onSubmit = (data) => {
-        console.log(data);
+    const onSubmit = async (data) => {
+        // Create the user object based on the provided structure
+        const userData = {
+            userId: data.idNumber,
+            name: data.firstName + " " + data.lastName, // Combining first and last name
+            idNumber: data.idNumber,
+            email: data.email,
+            password: data.password,
+            phoneNumber: data.phoneNumber,
+            role: data.role,
+            profilePicture: "", // You can set this to a default value or update it later
+        };
+
+        try {
+            const response = await axios.post(`${API_URL}/UserDTO`, userData); // Replace YOUR_API_ENDPOINT with your actual endpoint
+            console.log("Usuario creado:", response.data);
+
+            // Handle successful response (e.g., show a success message, redirect, etc.)
+        } catch (error) {
+            console.error("Error al crear usuario:", error);
+            // Handle error (e.g., show an error message)
+        }
     };
 
     return (
-        <div className="container__form">
-            <form className="form" onSubmit={onSubmit}>
+        <div className="container__form-two">
+            <h1>Crear usuario</h1>
+            <form className="form" onSubmit={handleSubmit(onSubmit)}>
                 <div className="form__group">
                     <label className="form__label">Nombre:</label>
                     <input
                         className="form__input"
                         type="text"
-                        name="firstName"
                         {...register("firstName", {
                             required: "Nombre es requerido",
                             maxLength: { value: 30, message: "Nombre tiene que ser menor a 30 caracteres" },
@@ -57,13 +81,11 @@ export const CreateUserForm = () => {
                     <input
                         className="form__input"
                         type="text"
-                        name="lastName"
                         {...register("lastName", {
                             required: "Apellidos son requeridos",
-                            maxLength: { value: 50, message: "Apellidos tienen que ser menores a 30 caracteres" },
-                            minLength: {
-                                value: 2, message: "Apellidos tienen que ser al menos 2 caracteres"
-                            }, pattern: { value: /^[a-zA-Z\s]+$/, message: "Apellidos solo aceptan letras" }
+                            maxLength: { value: 50, message: "Apellidos tienen que ser menores a 50 caracteres" },
+                            minLength: { value: 2, message: "Apellidos tienen que ser al menos 2 caracteres" },
+                            pattern: { value: /^[a-zA-Z\s]+$/, message: "Apellidos solo aceptan letras" }
                         })}
                     />
                     {errors.lastName && <span className="form__error">{errors.lastName.message}</span>}
@@ -74,7 +96,6 @@ export const CreateUserForm = () => {
                     <input
                         className="form__input"
                         type="text"
-                        name="idNumber"
                         {...register("idNumber", {
                             required: "Número de cédula es requerido",
                             pattern: { value: /^[0-9]+$/, message: "Número de cédula debe ser solo numérico" }
@@ -88,7 +109,6 @@ export const CreateUserForm = () => {
                     <input
                         className="form__input"
                         type="tel"
-                        name="phoneNumber"
                         {...register("phoneNumber", {
                             required: "Número de teléfono es requerido",
                             pattern: { value: /^[0-9]{8}$/, message: "Número de teléfono debe ser de 8 dígitos" }
@@ -102,7 +122,6 @@ export const CreateUserForm = () => {
                     <input
                         className="form__input"
                         type="email"
-                        name="email"
                         {...register("email", {
                             required: "Correo electrónico es requerido",
                             pattern: {
@@ -119,7 +138,6 @@ export const CreateUserForm = () => {
                     <input
                         className="form__input"
                         type="password"
-                        name="password"
                         {...register("password", {
                             required: "Contraseña es requerida",
                             minLength: { value: 8, message: "Contraseña debe tener al menos 8 caracteres" },
@@ -132,13 +150,11 @@ export const CreateUserForm = () => {
                     {errors.password && <span className="form__error">{errors.password.message}</span>}
                 </div>
 
-
                 <div className="form__group">
                     <label className="form__label">Confirmación de contraseña:</label>
                     <input
                         className="form__input"
                         type="password"
-                        name="confirmPassword"
                         {...register("confirmPassword", {
                             required: "Confirmación de contraseña es requerida",
                             validate: (value) => value === password.current || "Contraseñas no coinciden"
@@ -150,18 +166,16 @@ export const CreateUserForm = () => {
                 <div className='form__group full-width'>
                     <label className="form__label">Rol:</label>
                     <select className='form__input' {...register("role")}>
-                        {
-                            roles.map((role) => (
-                                <option key={role.value} value={role.value}>{role.label}</option>
-                            ))
-                        }
+                        {roles.map((role) => (
+                            <option key={role.value} value={role.value}>{role.label}</option>
+                        ))}
                     </select>
                 </div>
 
                 <div className="form__button-wrapper">
-                    <button className="form__button" type="submit">Registrarme</button>
+                    <button className="form__button" type="submit">Crear usuario</button>
                 </div>
             </form>
         </div>
-    )
-}
+    );
+};
