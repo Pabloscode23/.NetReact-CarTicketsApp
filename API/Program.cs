d4e1230790e04526402982087824a20a7e2d5f55
Kendall Cordero Aguirre <kcorderoag@ucenfotec.ac.cr>
2024-10-29 20:26:10 -0600

    [Backend]Reestructua de las notificaciones

--- API/Program.cs
+++ API/Program.cs
@@ -25,8 +25,8 @@ namespace API
             var secretKey = Encoding.UTF8.GetBytes(jwtSettings["SecretKey"]);
 
             // Creacion de notificacion
-            builder.Services.AddScoped<INotification>(provider =>
-            NotificationFactory.CreateNotification("email"));
+            builder.Services.AddSingleton<INotification, EmailNotification>();
+            builder.Services.AddSingleton<NotificationFA>();
 
             builder.Services.AddAuthentication(options =>
             {
